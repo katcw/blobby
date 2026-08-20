ffb03ee9874af87dcbaa3df47f6077c04c972448
katcw <74291130+katcw@users.noreply.github.com>
2026-08-20 19:48:06 +0800

    Initial Commit

---
+++ blobby/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blobby
+//
+//  Created by kalen •ᴗ•  on 20/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blobby/blobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  blobbyApp.swift
+//  blobby
+//
+//  Created by kalen •ᴗ•  on 20/8/26.
+//
+
+import SwiftUI
+
+@main
+struct blobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
